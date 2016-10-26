67899afe386bb70509d27548bcd5f16b21c083ae
WangZishi <ynh.2@outlook.com>
2016-10-26 15:46:19 +0800

    add download with zip pack

--- app.ts
+++ app.ts
@@ -1,17 +1,18 @@
 import * as Koa from 'koa';
 import * as _ from 'lodash';
-import { createWriteStream, createReadStream, unlink, renameSync, readdirSync } from 'fs';
+import { createWriteStream, createReadStream, unlink, renameSync, readdirSync, readFileSync } from 'fs';
 
 const multer = require('koa-multer');
 const router = require('koa-router');
 const send = require('koa-send');
 const Convert = require('koa-convert');
+const JSZip = require('jszip');
 
 const upload = multer({ dest: 'uploads/' });
 const tmp = multer({ dest: 'tmp/' });
 
 const app = new Koa();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 const CORSConfig = {
     origin: '*',
@@ -32,6 +33,18 @@ app.use(router().get('/api/v3/file/:dir/:fileName', async (ctx, next) => {
         fileName = ctx.params.fileName;
     ctx.set('Content-Disposition', `attachment; filename*= UTF-8''${encodeURIComponent(fileName)}`);
     await send(ctx, `${dir}/${fileName}`);
+}).routes());
+
+// GET /api/v3/file/uploads?file=1.json&file=2.json HTTP/1.1
+// Accept: application/zip
+app.use(router().get('/api/v3/file/:dir', async (ctx, next) => {
+    if (ctx.get('Accept') !== 'application/zip') return await next();
+    let dir = ctx.params.dir === 'upload' ? 'uploads' : 'tmp';
+    let fileNames = ctx.query.fileNames;
+    let zip = fileNames.reduce((zip, fileName) => zip.file(fileName, readFileSync(`uploads/${fileName}`)), new JSZip());
+
+    ctx.set('Content-Disposition', `attachment; filename*= UTF-8''${encodeURIComponent('Archive.zip')}`);
+    ctx.body = zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true });
 }).routes());
 
 app.use(router().post('/api/v3/file/:dir', async (ctx, next) => {
@@ -78,7 +91,7 @@ app.use(router().get('/api/v3/file/check/:dir/:fileName', async (ctx, next) => {
     filenames.forEach(filename => {
         if (filename === fileName) isExist = true;
     });
-    
+
     ctx.body = isExist;
 }).routes());
 
